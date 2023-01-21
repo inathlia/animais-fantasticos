7ec5285eade41d51a24939a0ab35c95f1f2571ca
inathlia <nathtenaglia@hotmail.com>
2023-01-21 12:44:21 -0300

    animacao-numeros

---
+++ js/modules/funcionamento.js
@@ -0,0 +1,23 @@
+export default function initFuncionamento(){
+    const funcionamento = document.querySelector('[data-semana]');
+    //pega o valor do data-semana/data-horario e transforma em um array pelo método .split
+    //para transformar em números basta usar o .map e o objeto Number
+    const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
+    const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+
+    const dataAgora = new Date();
+    const diaAgora = dataAgora.getDay();
+    const horarioAgora = dataAgora.getHours();
+
+    //verificar se o dia de hoje está dentro do array
+    const semanaAberto = diasSemana.indexOf(diaAgora) !== -1; 
+    //verificar se o horario atual está válido
+    const horarioAberto = horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1];
+
+    if(semanaAberto && horarioAberto){
+        funcionamento.classList.add('aberto');
+    }
+    else{
+        funcionamento.classList.remove('aberto');
+    }
+}
--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
+import initFuncionamento from "./modules/funcionamento.js";
 
 initTabNav();
 initAccordion();
@@ -13,3 +15,5 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
