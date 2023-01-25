9476f3062a8993324bec4896c783c67fc2e77fc8
inathlia <nathtenaglia@hotmail.com>
2023-01-25 16:39:08 -0300

    fetch

--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
 initTabNav();
 initAccordion();
@@ -15,5 +16,6 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
