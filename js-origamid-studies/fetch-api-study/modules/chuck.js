138608df01c55882ef6a97daa3443c7512d0ada4
inathlia <nathtenaglia@hotmail.com>
2023-01-25 15:01:31 -0300

    studies

---
+++ js-origamid-studies/fetch-api-study/modules/chuck.js
@@ -0,0 +1,15 @@
+export default function initChuck(){
+    const btnProxima = document.querySelector('.proxima');
+    const paragrafoPiada = document.querySelector('.piada');
+
+    function puxarPiada(){
+        fetch(`https://api.chucknorris.io/jokes/random`)
+        .then(response => response.json())
+        .then(piada => {
+            paragrafoPiada.innerText = piada.value;
+        })
+    }
+
+    btnProxima.addEventListener('click', puxarPiada);
+}
+
